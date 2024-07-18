06c077a192d2d7c51f2bb4a2a96155bff368fdb2
Juan.DiazUAO <150868565+juandiaz9705@users.noreply.github.com>
2024-07-17 19:48:28 -0500

    Update load_model.py
    Comentario

--- load_model.py
+++ load_model.py
@@ -3,7 +3,7 @@ from tensorflow.keras import models
 
 def model():
     try:
-        model_cnn = tf.keras.models.load_model('C:/UAO/UAO-Neumonia/WilhemNet_86.h5')
+        model_cnn = tf.keras.models.load_model('C:/UAO/UAO-Neumonia/WilhemNet_86.h5')#La ruta debe direccionar en donde esta el modelo de forma local.
         return model_cnn
     except OSError as e:
         print(f"Error loading model: {e}")
